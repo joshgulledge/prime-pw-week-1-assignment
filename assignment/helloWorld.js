8baab8b8fbd4c00a9c209b9887387180a1a94941
Joshua Gulledge <71677807+joshgulledge@users.noreply.github.com>
2020-12-04 10:30:25 -0600

    tweaking javascript

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,4 +1,5 @@
 "use strict"
+const instructorName = prompt("Please type your name");
 
-console.log("Hello Instructors");
+console.log(`Hello ${instructorName}`);
 console.log("I mean world... Hello World");
